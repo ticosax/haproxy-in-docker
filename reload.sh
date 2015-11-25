3a2715e0b327de4c8c444652f9794157b3618502
Nicolas Delaby <nicolas.delaby@lock8.me>
2015-11-25 20:08:28 +0100

    High availabilty
    http://engineeringblog.yelp.com/2015/04/true-zero-downtime-haproxy-reloads.html

--- reload.sh
+++ reload.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+set -e
 
+iptables -I INPUT -p tcp --dport 8080 --syn -j DROP
+sleep 0.2
 haproxy -f /etc/haproxy/haproxy.cfg -p /run/haproxy.pid -sf $(cat /run/haproxy.pid)
+iptables -D INPUT -p tcp --dport 8080 --syn -j DROP
